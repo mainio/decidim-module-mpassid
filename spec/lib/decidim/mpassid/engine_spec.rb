6f1ae81b2ed845b0dc805e65034129d46e5d959f
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-07-07 14:49:03 +0300

    Remove specs testing removed code

--- spec/lib/decidim/mpassid/engine_spec.rb
+++ spec/lib/decidim/mpassid/engine_spec.rb
@@ -113,48 +113,12 @@ describe Decidim::Mpassid::Engine do
     run_initializer("decidim_mpassid.setup")
   end
 
-  it "calls the add_omniauth_provider method correctly" do
-    expect(described_class).to receive(:add_omniauth_provider)
-    expect(ActiveSupport::Reloader).to receive(:to_run) do |&block|
-      expect(described_class).to receive(:add_omniauth_provider)
-      block.call
-    end
-
-    run_initializer("decidim_mpassid.omniauth_provider")
-  end
-
   it "adds the mail interceptor" do
     expect(ActionMailer::Base).to receive(:register_interceptor).with(
       Decidim::Mpassid::MailInterceptors::GeneratedRecipientsInterceptor
     )
 
     run_initializer("decidim_mpassid.mail_interceptors")
-  end
-
-  describe "#add_omniauth_provider" do
-    it "adds the :mpassid OmniAuth provider" do
-      # Reset the constant
-      original_providers = [:facebook, :twitter, :google_oauth2]
-      ::Decidim::User.send(:remove_const, :OMNIAUTH_PROVIDERS)
-      ::Decidim::User.const_set(:OMNIAUTH_PROVIDERS, original_providers)
-
-      expected = original_providers + [:mpassid]
-
-      expect(::Decidim::User).to receive(:remove_const).once.with(
-        :OMNIAUTH_PROVIDERS
-      ).and_call_original
-      expect(::Decidim::User).to receive(:const_set).once.with(
-        :OMNIAUTH_PROVIDERS,
-        expected
-      ).and_call_original
-
-      # Make sure that the constant monkey patch is only done once even when the
-      # to_prepare hooks are run multiple times.
-      described_class.add_omniauth_provider
-      described_class.add_omniauth_provider
-
-      expect(::Decidim::User::OMNIAUTH_PROVIDERS).to eq(expected)
-    end
   end
 
   def run_initializer(initializer_name)
